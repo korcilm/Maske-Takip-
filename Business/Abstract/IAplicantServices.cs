24ec24bac7108b7a080e06d67cce7d2222ecb862
korcilm <58364191+korcilm@users.noreply.github.com>
2020-09-07 21:33:52 +0300

    Add files via upload

---
+++ Business/Abstract/IAplicantServices.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IAplicantServices
+    {
+        void ApplyForMask(Person person);
+        List<Person> GetList();
+        bool CheckPerson(Person person);
+    }
+}
---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class PttManager :ISuplierService
+    {
+        private IAplicantServices _aplicantServices;
+
+        public PttManager(IAplicantServices aplicantServices)
+        {
+            _aplicantServices = aplicantServices;
+        }
+        public void GiveMask(Person person)
+        {
+            if(_aplicantServices.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske Verilemedi");
+            }
+        }
+    }
+}
